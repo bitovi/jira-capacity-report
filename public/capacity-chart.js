1714ec0e8c5c98b5b5e3ed050934362613bb6b2d
Jan Jorgensen <jandjorgensen@gmail.com>
2022-12-29 11:55:03 -0700

    BITOVI-17 epics per week graph

--- public/capacity-chart.js
+++ public/capacity-chart.js
@@ -1,13 +1,58 @@
 import { StacheElement, type } from "//unpkg.com/can@6/core.mjs";
 import './chart.js';
 
+export class CapacityChartCanvas extends StacheElement {
+    static view = `
+        <canvas id="capacityChart" width="400" height="400"></canvas>
+    `
+
+    connected() {
+        this.chartRef = this.querySelector('#capacityChart');
+        if (this.chartData) {
+            this.chart = new Chart(this.chartRef, this.chartData);
+        }
+
+        return () => {
+            this.chart.destroy();
+        }
+    }
+
+    static props = {
+        chart: {
+            type: type.maybe(Object),
+        },
+        chartRef: {
+            type: type.maybe(Object),
+        },
+        chartData: {
+            type: type.maybe(Object),
+            set(newVal, oldVal) {
+                if (this.chartRef) {
+                    if (!oldVal) {
+                        this.chart = new Chart(this.chartRef, newVal);
+                    } else {
+                        this.chart.data = newVal.data;
+                        this.chart.update();
+                    }
+                }
+
+                return newVal;
+            }
+        }
+    }
+}
 
 export class CapacityChart extends StacheElement {
 
     static view = `
+    <div class="container">
         <h2>Capacity Chart</h2>
-        {{chartData}}
-        <canvas id="capacityChart" width="400" height="400"></canvas>
+        <div class="row">
+            <div class="col-6">
+                <capacity-chart-canvas chartData:from="chartData"></capacity-chart-canvas>
+            </div>
+        </div>
+    </div>
     `;
 
     static props = {
@@ -20,38 +65,50 @@ export class CapacityChart extends StacheElement {
         epicsBetweenDates: {
             type: type.maybe(Array),
             default: [],
-        }
-    }
-
-    // when this is finally defined, we need to call something like:
-    // new Chart(this.querySelector('#capacityChart'), this.chartData)
-    get chartData() {
-        if (this.countEpicsAsDateRangePerWeekRange && this.labels) {
-            const result = {
-                type: 'line',
-                data: {
-                    labels: this.labels,
-                    datasets: [{
-                        label: 'Epics per week',
-                        data: this.countEpicsAsDateRangePerWeekRange,
-                        fill: false,
-                        borderColor: 'rgb(75, 192, 192)',
-                        tension: 0.1
-                    }]
+        },
+        labels: {
+            get() {
+                if (this.weekRanges) {
+                    return this.weekRanges.map(week => {
+                        return week.start.toLocaleDateString("en-US");
+                    });
                 }
             }
-            console.log(result)
-            return result;
+        },
+        chartData: {
+            value({ listenTo, lastSet, resolve }) {
+                console.log
+                listenTo('labels', () => {
+                    console.log('labels changed')
+                    resolve(this.chartData);
+                });
+                listenTo('countEpicsAsDateRangePerWeekRange', () => {
+                    console.log('countEpicsAsDateRangePerWeekRange changed')
+                    resolve(this.chartData);
+                });
+            },
+            get() {
+                if (this.countEpicsAsDateRangePerWeekRange && this.labels) {
+                    const result = {
+                        type: 'line',
+                        data: {
+                            labels: this.labels,
+                            datasets: [{
+                                label: 'Epics in Flight During Week',
+                                data: this.countEpicsAsDateRangePerWeekRange,
+                                fill: false,
+                                borderColor: 'rgb(75, 192, 192)',
+                                tension: 0.1
+                            }]
+                        }
+                    }
+                    console.log(result)
+                    return result;
+                }
+            }
         }
     }
 
-    get labels() {
-        if (this.weekRanges) {
-            return this.weekRanges.map(week => {
-                return week.start.toLocaleDateString("en-US");
-            });
-        }
-    }
 
     get countEpicsAsDateRangePerWeekRange() {
         if (this.epicsBetweenDates && this.weekRanges) {
@@ -99,5 +156,5 @@ function addDays(date, days) {
     copy.setDate(date.getDate() + days);
     return copy;
 }
-
+customElements.define('capacity-chart-canvas', CapacityChartCanvas);
 customElements.define('capacity-chart', CapacityChart);
